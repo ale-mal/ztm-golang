8e5692e95a856577399b8095868576ba17711008
AlexandrMalishev <alexandr.malishev@mytona.com>
2022-12-04 12:17:36 +0700

    Functions

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
+func fibonacci(num uint) uint {
+	if num <= 1 {
+		return num
+	}
+	return fibonacci(num-1) + fibonacci(num-2)
+}
 
+func main() {
+	for i := uint(0); i <= 10; i++ {
+		fmt.Println("i:", i, ", fibonacci is:", fibonacci(i))
+	}
 }
--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -3,14 +3,6 @@
 //  information to the terminal.
 //
 //--Requirements:
-//* Write a function that accepts a person's name as a function
-//  parameter and displays a greeting to that person.
-//* Write a function that returns any message, and call it from within
-//  fmt.Println()
-//* Write a function to add 3 numbers together, supplied as arguments, and
-//  return the answer
-//* Write a function that returns any number
-//* Write a function that returns any two numbers
 //* Add three numbers together using any combination of the existing functions.
 //  * Print the result
 //* Call every function at least once
@@ -19,6 +11,43 @@ package main
 
 import "fmt"
 
-func main() {
+//* Write a function that accepts a person's name as a function
+//  parameter and displays a greeting to that person.
+func greet(name string) {
+	fmt.Println("greetins,", name, "!")
+}
 
+//* Write a function that returns any message, and call it from within
+//  fmt.Println()
+func getAnyMessage() string {
+	return "any message"
+}
+
+//* Write a function to add 3 numbers together, supplied as arguments, and
+//  return the answer
+func tripleSum(a, b, c int) int {
+	return a + b + c
+}
+
+//* Write a function that returns any number
+func getAnyNumber() int {
+	return 8
+}
+
+//* Write a function that returns any two numbers
+func getAnyTwoNumbers() (int, int) {
+	return 1, 2
+}
+
+func main() {
+	greet("han yolo")
+
+	fmt.Println(getAnyMessage())
+
+	one, two, three := 1, 2, 3
+	fmt.Println("sum of ", one, two, three, "is", tripleSum(one, two, three))
+
+	one, two = getAnyTwoNumbers()
+	three = getAnyNumber()
+	fmt.Println("sum of ", one, two, three, "is", tripleSum(one, two, three))
 }

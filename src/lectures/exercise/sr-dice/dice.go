a028a32dc5cbd5a7e1cd65cdf56b21df7bf67df7
AlexandrMalishev <alexandr.malishev@mytona.com>
2022-12-04 20:33:36 +0700

    Fundamentals end: Dice roller

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -23,7 +23,41 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	dice, sides := 2, 12
+	rolls := 1
+
+	for r := 1; r <= rolls; r++ {
+		sum := 0
+
+		for d := 1; d <= dice; d++ {
+			num := roll(sides)
+			sum += num
+			fmt.Println("Roll #", r, ", Die #", d, ":", num)
+		}
+
+		fmt.Println("Total rolled:", sum)
+		switch {
+		case sum == 2 && dice == 2:
+			fmt.Println("Snake eyes!")
+		case sum == 7:
+			fmt.Println("Lucky 7")
+		case sum%2 == 0:
+			fmt.Println("Even")
+		case sum%2 == 1:
+			fmt.Println("Odd")
+		}
+	}
 }

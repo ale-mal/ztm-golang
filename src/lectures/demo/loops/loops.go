146966a06d1457a8c4a650863efa36d8d2a1ff90
AlexandrMalishev <alexandr.malishev@mytona.com>
2022-12-04 20:22:27 +0700

    Loops

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("sum is", sum)
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("sum is", sum)
+	}
 
+	fmt.Println("decrement")
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("sum is", sum)
+	}
 }
--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,16 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 50; i++ {
+		div3, div5 := i%3 == 0, i%5 == 0
+		if div3 && div5 {
+			fmt.Println("fizzbuzz")
+		} else if div3 {
+			fmt.Println("fizz")
+		} else if div5 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }

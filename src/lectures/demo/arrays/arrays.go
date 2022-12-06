dd446a4dd129be5a402f9e00a14728ac8ae09a3c
AlexandrMalishev <alexandr.malishev@mytona.com>
2022-12-06 23:09:48 +0700

    Arrays

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Perform cleaning...")
+	for i := 0; i < len(rooms); i++ {
+		j := i
+		rooms[j].cleaned = true
+	}
+
+	checkCleanliness(rooms)
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,49 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func printProducts(products [4]Product) {
+	var totalNumber int
+	var totalCost int
+	var lastIndex int
+	lastIndex = -1
+
+	for i := 0; i < len(products); i++ {
+		j := i
+
+		if products[j].name == "" {
+			continue
+		}
+
+		totalNumber++
+		totalCost += products[j].price
+		lastIndex = j
+	}
+
+	if lastIndex == -1 {
+		fmt.Println("Products are empty!")
+		return
+	}
+
+	fmt.Println("Total number is", totalNumber)
+	fmt.Println("Total cost is", totalCost)
+	fmt.Println("Last item name is", products[lastIndex])
+}
+
+func main() {
+	var products [4]Product
+
+	products[0] = Product{"Milk", 60}
+	products[1] = Product{"Banana", 10}
+	products[2] = Product{"Onigiri", 20}
+
+	printProducts(products)
+
+	products[3] = Product{"Whiskey", 900}
+
+	printProducts(products)
 }

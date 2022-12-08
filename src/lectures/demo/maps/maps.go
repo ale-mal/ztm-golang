54a891905e7b3539e1f0ac2b8a66738f75a74af1
AlexandrMalishev <alexandr.malishev@mytona.com>
2022-12-08 22:13:49 +0700

    Maps

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 10
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted, shopping list after:", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	if !found {
+		fmt.Println("Cereal not found")
+	} else {
+		fmt.Println("Cereal found, count is", cereal)
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("Total items is", totalItems)
 }
--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -12,7 +12,6 @@
 //* Set all of the server statuses to `Online` when creating the map
 //* After creating the map, perform the following actions:
 //  - call display server info function
-//  - change server status of `darkstar` to `Retired`
 //  - change server status of `aiur` to `Offline`
 //  - call display server info function
 //  - change server status of all servers to `Maintenance`
@@ -29,6 +28,40 @@ const (
 	Retired     = 3
 )
 
+func printStatuses(statuses map[string]int) {
+	fmt.Println("Total num is", len(statuses))
+
+	stats := map[int]int{}
+	for _, status := range statuses {
+		stats[status]++
+	}
+
+	fmt.Println("Online num is", stats[Online])
+	fmt.Println("Offline num is", stats[Offline])
+	fmt.Println("Maintenance num is", stats[Maintenance])
+	fmt.Println("Retired num is", stats[Retired])
+
+	fmt.Println()
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serverStatus := make(map[string]int)
+	for _, server := range servers {
+		serverStatus[server] = Online
+	}
+
+	printStatuses(serverStatus)
+
+	serverStatus["darkstar"] = Retired
+	serverStatus["aiur"] = Offline
+
+	printStatuses(serverStatus)
+
+	for server, _ := range serverStatus {
+		serverStatus[server] = Maintenance
+	}
+
+	printStatuses(serverStatus)
 }

0d153794a2177128710777f1ec6774c509c0458c
AlexandrMalishev <alexandr.malishev@mytona.com>
2023-01-05 18:29:03 +0700

    Interfaces

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("chop salad")
+	fmt.Println("add dressing")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for i := 0; i < len(dishes); i++ {
+		fmt.Printf("--Dish: %v--\n", dishes[i])
+		dishes[i].PrepareDish()
+	}
+	fmt.Println()
+}
+
+func main() {
+	dishes := []Preparer{
+		Chicken("Chicken wings"),
+		Salad("Caesar salad"),
+	}
+	prepareDishes(dishes)
 }

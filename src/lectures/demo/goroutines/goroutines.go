242b0fddf2b6967937ecf36a1590ac75a3582a44
AlexandrMalishev <alexandr.malishev@mytona.com>
2023-01-14 16:08:05 +0700

    Goroutines

--- src/lectures/demo/goroutines/goroutines.go
+++ src/lectures/demo/goroutines/goroutines.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
+	capitalized := data
+
+	for i := 0; i < len(data); i++ {
+		// use channel to prevent data races
+		ch := make(chan rune)
+		go func() {
+			ch <- unicode.ToUpper(data[i])
+		}()
+		capitalized[i] = <-ch
+	}
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("Capitalized: %c\n", capitalized)
 }
